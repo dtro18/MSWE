8172f9dd8b617f2fcb5456681f996d266d9b262f
Dylan <dtro18@gmail.com>
2024-09-30 21:55:42 -0700

    Functionality for Arithmetic

--- Data Structures/Assignment 2/module/assignment2.py
+++ Data Structures/Assignment 2/module/assignment2.py
@@ -25,7 +25,7 @@ class Stack:
     def pop(self): # Removes the latest element from the stack and returns it. 
         returnedNode = self.head
         self.head = self.head.next
-        return returnedNode
+        return returnedNode.element
     
     def peek(self): # returns the latest element from the stack without removing it 
         return self.head.element
@@ -44,46 +44,80 @@ class Stack:
         while cur: 
             print(cur.element) 
             cur = cur.next
-    
+    # Helper function for the arithmetic eval. 
+    def reverseStack(self):
+        tempStack1 = Stack() 
+        
+        while not self.is_empty():
+            tempStack1.push(self.pop())
+        tempStack2 = Stack()
+        while not tempStack1.is_empty():
+            tempStack2.push(tempStack1.pop())
+        while not tempStack2.is_empty():
+            self.push(tempStack2.pop())
+
     # Input: str = "10 + 20 * 2"
     # Output: 50
     # Input: str = “foo / 30 + 7”
     # Output: NaN
     def arithmeticEval(self, string):
         # Preprocessing
-        num = ''
+        
         i = 0
+        num = ''
         while i < len(string):
             char = string[i]
             if char == " ":
                 i += 1
                 continue
+            elif char not in "1234567890*/+- ":
+                raise ValueError("NaN")
             elif (char in "+-"):
-                # Reached punctuation, snapshot whatever's in num
-                self.push(num)
                 self.push(char)
-                num = ''
+                i += 1
+                continue
             elif (char in "*/"):
-                firstVal = int(num)
-                # Finding second value
-                num = ''
+                firstVal = int(self.pop())
+                # Finding second value 
                 i += 1
                 while i < len(string):
                     if string[i] in "1234567890":
-                        num += string[i]
-                    i += 1
-                secondVal = int(num)
-
+                        while i < len(string) and string[i] != " ":
+                            num += string[i]
+                            i += 1
+                        break
+                    else:
+                        i += 1
+                secondVal = int(num) 
+                # Reset the numstring once we've found second value
+                num = ''
+                # Carry out the */ operations
                 if char == '*':
                     self.push(str(firstVal * secondVal))
-                    continue
                 if char == "/":
                     self.push(str(firstVal // secondVal))
             else:
-                num += char
-            i += 1
-        self.push(num)
-print("TEST")
+                # Found a number
+                while i < len(string) and string[i] != " ":
+                    num += string[i]
+                    i += 1
+                self.push(num)
+                num = ''
+
+        self.reverseStack()
+        self.print()
+
+        while self.size() > 1:
+            secondVal = int(self.pop())
+            operand = self.pop() # + or -
+            firstVal = int(self.pop())
+            if operand == "+":
+                self.push(str(firstVal + secondVal))
+            elif operand == "-":
+                self.push(str(secondVal - firstVal))
+        # return self.peek()
+
 stack = Stack()
-stack.arithmeticEval("10 + 20 * 2")
+stack.arithmeticEval("10 * 10 * 10 + 150 / 2")
+# 10 + 40 + 10 - 2 - 14
 stack.print()
