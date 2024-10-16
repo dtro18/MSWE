60786f46ba86cd288446528b0d3b1c534fcf53e3
Dylan <dtro18@gmail.com>
2024-10-16 15:34:26 -0700

    Finished assignment2

--- Applied DSA/Assignment 2/module/assignment2.py
+++ Applied DSA/Assignment 2/module/assignment2.py
@@ -1,7 +1,7 @@
 # Implementing binary search
+# Referenced leetcode question binary search on a 2d array.
 
-
-def binarySearch(arr, val):
+def binarySearch1d(arr, val):
     l, r = 0, len(arr) - 1
 
     res = [-1, -1]
@@ -28,3 +28,38 @@ def binarySearch(arr, val):
         elif val > arr[m]:
             l = m + 1
     return res
+
+# matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+def binarySearch2d(arr, val):
+    if len(arr) == 0 or len(arr[0]) == 0:
+        return False
+    ROWS = len(arr)
+    COLS = len(arr[0])
+
+    # Identify row that it could be in
+    top, bottom = 0, ROWS - 1
+    while top <= bottom:
+        # Row is the row we're currently looking at
+        row = (top + bottom) // 2
+
+        if (val >= arr[row][0]) and (val <= arr[row][-1]):
+            # Move on
+            break
+        elif val < arr[row][0]:
+            bottom = row - 1
+        elif val > arr[row][-1]:
+            top = row + 1
+    
+    # Perform 1d binary search
+    l, r = 0, COLS - 1
+
+    while l <= r:
+        col = (l + r) // 2
+        if arr[row][col] == val:
+            return True
+        elif val < arr[row][col]:
+            r = col - 1
+        elif val > arr[row][col]:
+            l = col + 1
+    return False
+
--- Applied DSA/Assignment 2/test_assignment2.py
+++ Applied DSA/Assignment 2/test_assignment2.py
@@ -1,11 +1,26 @@
 import unittest
-from module.assignment2 import binarySearch
+from module.assignment2 import binarySearch1d, binarySearch2d
 
 # Testing stack functions for Task 1.
 class TestTask1(unittest.TestCase):
-    def test_hashFn(self):
+    def test_binarySearch1d(self):
         # Ensures that the hash function returns diff idx for diff strings
-        self.assertEqual(binarySearch([4,9,10,13,17,17,19,21], 17), [4, 5])
-        self.assertEqual(binarySearch([2,4,6,8,10,14,16], 12), [-1, -1])
-        self.assertEqual(binarySearch([], 0), [-1, -1])
+        self.assertEqual(binarySearch1d([4,9,10,13,17,17,19,21], 17), [4, 5])
+        self.assertEqual(binarySearch1d([2,4,6,8,10,14,16], 12), [-1, -1])
+        self.assertEqual(binarySearch1d([], 0), [-1, -1])
 
+class TestTask2(unittest.TestCase):
+    def test_binarySearch2d(self):
+        matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+        self.assertEqual((binarySearch2d(matrix, 3)), True)
+        self.assertEqual((binarySearch2d(matrix, 13)), False)
+        # See if can find with duplicate arrays
+        matrix = [[1,3,5,7],[11,11,16,20],[23,30,34,60]]
+        self.assertEqual((binarySearch2d(matrix, 11)), True)
+
+        # Testing search on empty arrays
+        matrix = [[]]
+        self.assertEqual((binarySearch2d(matrix, 0)), False)
+        matrix = []
+        self.assertEqual((binarySearch2d(matrix, 0)), False)
+    
