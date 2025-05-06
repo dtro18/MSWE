b53013b7f980f874fcd97b012337ce66b7bc4c02
Dylan <dtro18@gmail.com>
2025-05-06 00:22:11 -0700

    Working week 5

--- programming_styles/week5_exercise/JarClasses.java
+++ programming_styles/week5_exercise/JarClasses.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -33,54 +36,86 @@ public class JarClasses {
                     classNames.add(className);
                 }
             }
-        System.out.println(classNames);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return classNames;
     }
+    public static Class<?> getClassFromString(URLClassLoader classLoader, String className) {
+        try {
+            return classLoader.loadClass(className);
+        } catch (Throwable t) {
+            System.err.println("Skipping class: " + className + " due to " + t.getClass().getSimpleName());
+            return null;
+        }
+    }
 
-    public static Method[] getMethodsOfClass(String jarPath, String className) {
-        try (URLClassLoader classLoader = new URLClassLoader(
-                new URL[] { new File(jarPath).toURI().toURL() })) {
-            
-            Class<?> clazz = classLoader.loadClass(className);
-            return clazz.getDeclaredMethods();
-            
+    public static Method[] getMethodsOfClass(Class classPtr) {
+        try {
+            return classPtr.getDeclaredMethods();
         } catch (Throwable t) {
             // Catching Throwable to handle all issues, including linkage errors
-            System.err.println("Skipping class: " + className + " due to " + t.getClass().getSimpleName());
+            System.err.println("Skipping class2: " + classPtr + " due to " + t.getClass().getSimpleName());
             return new Method[0];
         }
     }
 
-    public static void main(String[] args) {
-        File file = new File("output.txt");
-        try {
-            PrintStream printStream = new PrintStream(file);
-            System.setOut(printStream);
+    public static void main(String[] args) {        
+        Set<String> classSet = getClassNames(args[0]);
+        List<String> classNames = new ArrayList<>(classSet);
+        Collections.sort(classNames);
+        try (URLClassLoader classLoader = new URLClassLoader(new URL[] { new File(args[0]).toURI().toURL() });) {
+            for (String className : classNames) {
+                try {
+                    int publicCount = 0;
+                    int privateCount = 0;
+                    int protectedCount = 0;
+                    int staticCount = 0;
+                    int fieldCount = 0;
 
+                    Class<?> clazz = getClassFromString(classLoader, className);
+                    
+                    if (clazz != null) {
+                        fieldCount = clazz.getDeclaredFields().length;
+                        for (Method method : getMethodsOfClass(clazz)) {
+                            int modifiersInt = method.getModifiers();
+                            String[] modifierList = null;
+                            try {
+                                String modifierStr = Modifier.toString(modifiersInt);
+                                modifierList = modifierStr.split(" ");
+                            } catch (Exception e) {
+                                System.out.println("Method doesn't exist");
+                            }
+                            for (String modName : modifierList) {
+                                if (modName.equals("public")) {
+                                    publicCount += 1;
+                                } else if (modName.equals("private")) {
+                                    privateCount += 1;
+                                } else if (modName.equals("protected")) {
+                                    protectedCount += 1;
+                                } else if (modName.equals("static")) {
+                                    staticCount += 1;
+                                }
+                            }
+                        }
+                    }
+                    
+                    System.out.println("----------" + className + "----------");
+                    System.out.println("  Public methods: " + publicCount);
+                    System.out.println("  Private methods: " + privateCount);
+                    System.out.println("  Protected methods: " + protectedCount);
+                    System.out.println("  Static methods: " + staticCount);
+                    System.out.println("  Fields: " + fieldCount);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("Couldn't find class");
+                }
+               
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         
-        // Redirect standard output to the file
-        
-        Set<String> classNames = getClassNames(args[0]);
-        System.out.println(classNames);
-        for (String className : classNames) {
-            System.out.println(className);
-            System.out.println("\n");
-            for (Method method : getMethodsOfClass(args[0], className)) {
-                
-                try {
-                    System.out.println(method.toString());
-                } catch (Exception e) {
-                    System.out.println("Method doesn't exist");
-                }
-                
-            }
-        }
 
     
     }
